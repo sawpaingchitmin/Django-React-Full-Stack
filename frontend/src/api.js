033352c33415b40338b4369a02b3fca32b6b3c5d
Saw Paing Chit Min <jimmybright1998@gmail.com>
2025-08-15 12:14:48 +0630

    Update env and api file

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { ACCESS_TOKEN } from './constants';
 
-const apiUrl = "https://django-backend-4q5v.onrender.com";
+const apiUrl = "http://localhost:8000";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
